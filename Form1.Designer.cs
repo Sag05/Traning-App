6db500dfadfcb2bd8652a7b49bf7d49856a715dc
Sag0550 <50415834+Sag0550@users.noreply.github.com>
2020-09-15 15:22:45 +0200

    added everything

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.PathButton = new System.Windows.Forms.Button();
+            this.Question = new System.Windows.Forms.Label();
+            this.AnswerText = new System.Windows.Forms.TextBox();
+            this.CRICR = new System.Windows.Forms.Label();
+            this.CRA = new System.Windows.Forms.Label();
+            this.Check = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFile";
+            // 
+            // PathButton
+            // 
+            this.PathButton.Location = new System.Drawing.Point(12, 12);
+            this.PathButton.Name = "PathButton";
+            this.PathButton.Size = new System.Drawing.Size(75, 23);
+            this.PathButton.TabIndex = 1;
+            this.PathButton.Text = "Path";
+            this.PathButton.UseVisualStyleBackColor = true;
+            this.PathButton.Click += new System.EventHandler(this.PathButton_Click);
+            // 
+            // Question
+            // 
+            this.Question.AutoSize = true;
+            this.Question.Location = new System.Drawing.Point(13, 71);
+            this.Question.Name = "Question";
+            this.Question.Size = new System.Drawing.Size(49, 13);
+            this.Question.TabIndex = 2;
+            this.Question.Text = "Question";
+            // 
+            // AnswerText
+            // 
+            this.AnswerText.Location = new System.Drawing.Point(12, 112);
+            this.AnswerText.Name = "AnswerText";
+            this.AnswerText.Size = new System.Drawing.Size(504, 20);
+            this.AnswerText.TabIndex = 3;
+            this.AnswerText.TextChanged += new System.EventHandler(this.AnswerText_TextChanged);
+            // 
+            // CRICR
+            // 
+            this.CRICR.AutoSize = true;
+            this.CRICR.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.CRICR.Location = new System.Drawing.Point(12, 152);
+            this.CRICR.Name = "CRICR";
+            this.CRICR.Size = new System.Drawing.Size(88, 13);
+            this.CRICR.TabIndex = 4;
+            this.CRICR.Text = "Correct/Incorrect";
+            this.CRICR.Visible = false;
+            // 
+            // CRA
+            // 
+            this.CRA.AutoSize = true;
+            this.CRA.Location = new System.Drawing.Point(116, 152);
+            this.CRA.Name = "CRA";
+            this.CRA.Size = new System.Drawing.Size(79, 13);
+            this.CRA.TabIndex = 5;
+            this.CRA.Text = "Correct Answer";
+            this.CRA.Visible = false;
+            // 
+            // Check
+            // 
+            this.Check.Location = new System.Drawing.Point(12, 147);
+            this.Check.Name = "Check";
+            this.Check.Size = new System.Drawing.Size(75, 23);
+            this.Check.TabIndex = 6;
+            this.Check.Text = "Check";
+            this.Check.UseVisualStyleBackColor = true;
+            this.Check.Click += new System.EventHandler(this.Check_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(687, 261);
+            this.Controls.Add(this.Check);
+            this.Controls.Add(this.CRA);
+            this.Controls.Add(this.CRICR);
+            this.Controls.Add(this.AnswerText);
+            this.Controls.Add(this.Question);
+            this.Controls.Add(this.PathButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.Button PathButton;
+        private System.Windows.Forms.Label Question;
+        private System.Windows.Forms.TextBox AnswerText;
+        private System.Windows.Forms.Label CRICR;
+        private System.Windows.Forms.Label CRA;
+        private System.Windows.Forms.Button Check;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,52 @@ namespace Traningapp
 {
     public partial class Form1 : Form
     {
+        string[] Words;
+        int qnr = 0 ;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+
+        private void PathButton_Click(object sender, EventArgs e)
+        {
+            openFile.ShowDialog();
+            Words = File.ReadAllLines(openFile.FileName);
+
+            LoadNext();
+            PathButton.Visible = false;
+
+
+        }
+
+        private void LoadNext()
+        {
+            Question.Text = Words[qnr * 2];
+            qnr++;
+        }
+
+        private void AnswerText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Check_Click(object sender, EventArgs e)
+        {
+            LoadNext();
+            CRICR.Visible = true;
+            CRA.Visible = true;
+            AnswerText.Enabled = false;
+            Check.Visible = false;
+
+
+            if (AnswerText.Text.ToLower() == Words[qnr * 2 + 1].ToLower())
+            {
+
+            }
+
         }
     }
 }
